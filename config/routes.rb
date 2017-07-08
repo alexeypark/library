d1e07f4473579332b138f884c429e83ff173839a
Alexey Park <alexey.g.park@gmail.com>
2017-07-08 14:46:01 +0600

    Admin panel added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  root 'books#index'
+
   resources :authors
   resources :books
   resources :categories
-  root 'books#index'
+
+  namespace :admin do
+    root 'books#index'
+    resources :books
+    resources :categories
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
