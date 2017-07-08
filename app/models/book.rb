a0a1cf1fbeb4307203785887eb0cd49203acde03
Alexey Park <alexey.g.park@gmail.com>
2017-07-08 15:18:11 +0600

    Image uploader added

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :categories
   has_and_belongs_to_many :authors
   belongs_to :user
+  has_many :images
 
   mount_uploader :cover, CoverUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708075105) do
+ActiveRecord::Schema.define(version: 20170708091737) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20170708075105) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "attachment"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
